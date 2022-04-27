8de068913043850f73125a3f54b65f306f0fc5b6
Erick Milan <erickmilan1002@gmail.com>
2022-04-27 17:13:20 -0500

    button add

--- src/app/pages/products/product/product.component.ts
+++ src/app/pages/products/product/product.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output,EventEmitter } from '@angular/core';
+import { Product } from '../interfaces/product.interfaces';
 
 @Component({
   selector: 'app-product',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
-
+  @Input() product!: Product;
+  @Output() addToCart = new EventEmitter<Product>();
   constructor() { }
 
   ngOnInit(): void {
   }
+  onClick(): void {
+    console.log('click en en elbutton', this.product);
+    this.addToCart.emit(this.product);
+  }
+} 
 
-}
